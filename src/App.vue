20da31a3f4aa93de4b28a69e435ffe52a3c2fb33
Ilya Golovin <ilya.golovin.14.01.12@gmail.com>
2022-03-03 18:05:28 +0300

    Style: add first layout version

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+        <div class="elevator-shaft">
+            <div class="floor"></div>
+            <div class="floor"></div>
+            <div class="floor"></div>
+            <div class="floor"></div>
+            <div class="floor"><div class="elevator"></div></div>
+        </div>
+        <div class="elevator-controls"></div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+    display: grid;
+    width: 1200px;
+    height: 600px;
+    grid-template-columns: 1fr 2fr;
+    grid-template-rows: 1fr;
+    box-sizing: border-box;
+}
+.elevator-shaft {
+    box-sizing: inherit;
+    display: grid;
+    border: 1px solid black;
+    width: 100%;
+    height: 100%;
+}
+.floor {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+}
+.floor:not(:first-child):before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 100%;
+    border: 1px dashed blue;
+}
+.elevator {
+    background-color: aqua;
+    width: 100%;
+    height: 100%;
+}
+.elevator-controls {
+    box-sizing: inherit;
+    border: 1px solid tomato;
 }
 </style>
